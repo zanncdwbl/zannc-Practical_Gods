0ac2ee15e19a2f3696abb6a3f8a7cd219af2452e
Jen Guerra <southpawgeek@users.noreply.github.com>
2024-07-22 23:24:32 -0400

    remove the wrap

--- src/ready.lua
+++ src/ready.lua
@@ -7,33 +7,3 @@ modutil.mod.Path.Wrap("SetupMap", function(base)
 	})
 	base()
 end)
-
-modutil.mod.Path.Wrap("HeraSprintLink", function(base, functionArgs)
-	if functionArgs.Hijack then
-		-- not sure why i have to stuff these in here??
-		-- functionArgs.StartAngle = 180
-		-- functionArgs.Scatter = 20
-		-- functionArgs.ProjectileCap = 3
-
-		local triggerArgs = { ImpactAngle = 0 } -- uh how do we determine this
-		if game.CheckCooldown("SupportFire", 0.1667) then
-			local enemyId = game.GetClosest({
-				Id = game.CurrentRun.Hero.ObjectId,
-				DestinationName = "EnemyTeam",
-				IgnoreInvulnerable = true,
-				IgnoreHomingIneligible = true,
-				Distance = functionArgs.Radius,
-			})
-			-- game.CreateAnimation({
-			-- 	Name = functionArgs.Vfx,
-			-- 	DestinationId = game.CurrentRun.Hero.ObjectId,
-			-- })
-			if enemyId and game.ActiveEnemies[enemyId] and not game.ActiveEnemies[enemyId].IsDead then
-				local victim = game.ActiveEnemies[enemyId]
-				game.CheckSupportingFire(victim, functionArgs, triggerArgs)
-			end
-		end
-	else
-		base(functionArgs)
-	end
-end)
